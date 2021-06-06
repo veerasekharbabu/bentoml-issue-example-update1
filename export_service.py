c94552f34dd706bdabb63a8bfda601a5f7f646d4
veerasekharbabu <78962490+veerasekharbabu@users.noreply.github.com>
2021-06-06 19:50:10 +0530

    Update export_service.py

--- export_service.py
+++ export_service.py
@@ -1,5 +1,5 @@
-from mypackage.service import Service
-from mypackage1.model import MySVC
+from mypackage1.service import Service
+from mypackage2.model import MySVC
 
 service = Service()
 service.pack("model", MySVC())  # MySVC could be loaded from a checkpoint instead
