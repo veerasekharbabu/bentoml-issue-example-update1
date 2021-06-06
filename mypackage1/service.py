1abe49db5d8803b6cd0bcb51b320e5b6c89ceb4d
veerasekharbabu <78962490+veerasekharbabu@users.noreply.github.com>
2021-06-06 19:46:34 +0530

    Update service.py

--- mypackage1/service.py
+++ mypackage1/service.py
@@ -1,1 +1,13 @@
 
+from bentoml import api, env, artifacts, BentoService
+from bentoml.adapters import StringInput, JsonOutput
+from bentoml.frameworks.sklearn import SklearnModelArtifact
+
+
+@env(infer_pip_packages=True)
+@artifacts([SklearnModelArtifact("model")])
+class Service(BentoService):
+
+    @api(input=StringInput(), output=JsonOutput())
+    def say_hi(self, x):
+        return f"Hello {x}!"
