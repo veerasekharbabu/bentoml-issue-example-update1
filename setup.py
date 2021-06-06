247a7605ba6bdfca8a8bff42433ccdd0850a02c3
veerasekharbabu <78962490+veerasekharbabu@users.noreply.github.com>
2021-06-06 19:57:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='mypackage',
     version='0.0.1',
-    packages=['mypackage','mypackage1'],
+    packages=['mypackage1','mypackage2'],
     install_requires=[
         'bentoml',
         'scikit-learn',
